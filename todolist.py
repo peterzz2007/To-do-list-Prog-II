6fc7e9c12018f9595f9b330cff02fcb81bed33cd
peterzz2007 <peterusthebrotherus@gmail.com>
2026-04-10 10:45:54 +0300

    todolist.py
    Pievienots Main logs un funkcionalitāte parolēm

--- todolist.py
+++ todolist.py
@@ -6,13 +6,15 @@ def window(width, height):
     y = (root.winfo_screenheight() // 2) - (height // 2)
     root.geometry(f'{width}x{height}+{x}+{y}')
 
+loginindex = 0
 
 class login(tk.Frame):
     def __init__(self, master):
         super().__init__()
         self.master = master
-        self.master.title = ("To do list")
+        self.master.wm_title("To do list")
         self.master.resizable(False, False)
+
         window(500, 350)
         # Programmas nosaukums iekš loga
         label = tk.Label(self, text="Profila Izvēlne", font= ("Trebuchet", 14 ,"bold"))
@@ -20,7 +22,7 @@ class login(tk.Frame):
 
         #Pogas
 
-        loginz = tk.Button(self, text="Ielogoties profilā", font= "Trebuchet", width= 25)
+        loginz = tk.Button(self, text="Ielogoties profilā", font= "Trebuchet", width= 25, command= self.enteruser)
         loginz.place(x=260 ,y= 40)
 
         newuser = tk.Button(self, text="Izveidot jaunu profilu", font= "Trebuchet", width= 25, command= self.adduser)
@@ -48,7 +50,50 @@ class login(tk.Frame):
             self.list.insert(tk.END, row[1])
         conn.commit()
         conn.close()
+
+    def enteruser(self):
+        global loginindex
+        selected_index = self.list.curselection()
+        if selected_index:
+            index = selected_index[0]
+            getpass = self.list.get(index)
+            loginindex = index
+            conn = sqlite3.connect('todolist.db')
+            c = conn.cursor()
+            c.execute(''' SELECT  * FROM PROFILE WHERE PROFILE_NAME = ?''', (getpass,))
+            for row in c.fetchall():
+                self.psswrd = row[2]
+            conn.commit()
+            conn.close()
+            
+
+        if self.psswrd == None:
+            pass
+        else:
+            passwordtext = tk.Label(self, text="Ievadiet paroli", font= "Trebuchet")
+            passwordtext.place( x=260 ,y= 145)
+            self.password = tk.Entry(self, width= 25, font= "Trebuchet")
+            self.password.place(x= 260, y = 170)
+            backbtn = tk.Button(self, text="Ielogoties", font= "Trebuchet", width= 8, command= self.passwordsys)
+            backbtn.place(x= 405, y = 195)
+
+    def passwordsys(self):
+        self.entered_pass = self.password.get()
+        if self.psswrd == self.entered_pass:
+            for widget in self.winfo_children():
+                widget.destroy()
+                self.destroy()
+                MainWindow(self.master)
+            print("Password correct")
+        else:
+            print("Password incorrect")
+
+
+            
+        
     
+
+
     def deleteuserc(self):
         selected_index = self.list.curselection()
         if selected_index:
@@ -57,6 +102,7 @@ class login(tk.Frame):
             conn = sqlite3.connect('todolist.db')
             c = conn.cursor()
             c.execute(''' DELETE FROM PROFILE WHERE PROFILE_NAME = ?''', (userdelete,))
+            c.execute(''' DELETE FROM SQLITE_SEQUENCE WHERE NAME = 'PROFILE' ''')
             conn.commit()
             conn.close()
             self.list.delete(index)
@@ -67,12 +113,63 @@ class login(tk.Frame):
             widget.destroy()
         self.destroy()
         register(self.master)
+        
+class MainWindow(tk.Frame):
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
+        self.master.wm_title("To do list")
+        window(500, 350)
+
+        conn = sqlite3.connect('todolist.db')
+        c = conn.cursor()
+        c.execute(''' SELECT  * FROM PROFILE WHERE PROFILE_ID = ?''', (loginindex + 1,))
+        for row in c.fetchall():
+         x = row[1]
+        
+        conn.commit()
+        conn.close()
+        label = tk.Label(self, text=f"Ielogojies profilā:", font= ("Trebuchet", 15, "bold"))
+        label.place(x=300 ,y= 270)
+
+        username = tk.Label(self, text=f"{x}", font= ("Trebuchet", 15, "bold"))
+        username.place(x=300 ,y= 300)
+
+        todolist = tk.Listbox(self, width = 31, height= 15, font= "Trebuchet")
+        todolist.place(x=10, y=40)
+
+        addtask = tk.Button(self, text="Pievienot uzdevumu", font= "Trebuchet", width= 20 ,command= self.newtask)
+        addtask.place(x=300 ,y= 10)
+
+        edittask = tk.Button(self, text="Rediģēt uzdevumu", font= "Trebuchet", width= 20)
+        edittask.place(x=300 ,y= 50)
+
+        deletetask = tk.Button(self, text="Dzēst uzdevumu", font= "Trebuchet", width= 20)
+        deletetask.place(x=300 ,y= 90)
+
+        addnotes = tk.Button(self, text="Pievienot piezīmi", font= "Trebuchet", width= 20)
+        addnotes.place(x=300 ,y= 130)
+
+        self.username = tk.Entry(self, width= 31, font= "Trebuchet")
+        self.username.place(x= 10, y = 10)
+
+        self.place(x=0, y=0, width=500, height=350)
+
+
+    def popupBonus():
+            popupBonusWindow = tk.Tk()
+            popupBonusWindow.wm_title("Window")
+            labelBonus = Label(popupBonusWindow, text="Input")
+            labelBonus.grid(row=0, column=0)
+            
+        
+
 
 class register(tk.Frame):
     def __init__(self, master):
         super().__init__()
         self.master = master
-        self.master.title = ("To do list")
+        self.master.wm_title("To do list")
         
         window(315, 250)
         # Programmas nosaukums iekš loga
@@ -113,6 +210,15 @@ class register(tk.Frame):
         conn.close()
        
         print(f"Username is {val2} and the password is {val1}")
+    
+
+
+
+
+print(loginindex)
+root = tk.Tk()
+login(root)
+root.mainloop()
 
 
 
