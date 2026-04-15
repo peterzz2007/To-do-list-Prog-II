42c66a659c2e500675e0254f7a1978b2fba7c45d
peterzz2007 <peterusthebrotherus@gmail.com>
2026-04-15 12:46:28 +0300

    Prog II darbs 'To do list'

--- todolist.py
+++ todolist.py
@@ -204,7 +204,7 @@ class MainWindow(tk.Frame):
         edittask = tk.Button(self, text="Rediģēt uzdevumu", font= "Trebuchet", width= 20 ,command= self.taskeditor)
         edittask.place(x=300 ,y= 50)
 
-        completetask = tk.Button(self, text="Dzēst uzdevumu", font= "Trebuchet", width= 20, command= self.deletetaskc)
+        completetask = tk.Button(self, text="Izpildīt uzdevumu", font= "Trebuchet", width= 20, command= self.deletetaskc)
         completetask.place(x=300 ,y= 90)
 
         addnotes = tk.Button(self, text="Pievienot piezīmi", font= "Trebuchet", width= 20, command= self.noteseditor)
